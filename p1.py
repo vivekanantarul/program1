eef13ef316cd60f3bb739314532b2bfb801dc45f
sam smith <idiot@gmail.com>
2019-06-10 10:21:10 +0530

    program file

---
+++ p1.py
@@ -0,0 +1,115 @@
+####python program to calculate the average of numbers in a given list
+##n=int(input("enter the number of elements to be inserted: "))
+##a=[]
+##for i in range(0,n):
+##    elem=int(input("enter the element: "))
+##    a.append(elem)
+##avg=sum(a)/n
+##print("average of elements in the list",round(avg,2))
+
+
+####branching statement:
+####1.simple if statement:
+##a=5
+##if a==5:
+##    print("welcome")
+
+####2.if....else statement:
+##a=5
+##if a==4:
+##    print("welcome")
+##else:
+##    print("hello")
+
+####3.else if statement:
+##a=int(input("enter the value"))
+##if a==4:
+##    print("four")
+##elif a==5:
+##    print("five")
+##elif a==6:
+##    print("six")
+##else:
+##    print("hello")
+
+
+####4.nested if statement:
+##a=int(input("enter the value"))
+##b=int(input("enter the value"))
+##if a==4:
+##      if a>b:
+##          print("yes")
+##      else:
+##          print("no")
+##else:
+####    print("terminate")
+##    if a==2:
+##        print("two")
+
+####looping statement:
+####1.while loop:
+##counter=1
+##while  counter<=10:
+##    print("hello")
+##    counter+=1
+
+####counter=1
+##while True:
+##    print("hello")
+
+####nested while:
+##counter=1
+####newcounter=1
+##while counter<5:
+##    print("hello")
+##    newcounter=1
+##    while newcounter<5:
+##        print("welcome")
+##        newcounter+=1
+##    counter+=1
+
+####2.for loop:
+##var="welcome"
+##for x in var:
+##    print(x)
+
+##var="welcome"
+##for x in range(len(var)):
+##    print(x)
+
+##for x in range(10):
+##    print(x)
+
+##for x in range(1,10):
+##    print(x)
+
+##for x in range(1,10,2):
+##    print(x)
+
+####break statement:
+##i=0
+##while i<=10:
+##    print(i)
+##    i+=1
+##    if i==5:
+##        break
+##print("break statement is on")
+
+####continue statement:
+##i=0
+##while i<=10:
+##    i+=1
+##    if i==5:
+##        continue
+##    print(i)
+
+####pass statement:
+##i=0
+##if i==5:
+##    pass
+
+##for i in range (10):
+##    if i==5:
+##        pass
+##        print("pass statement")
+##    print("current i value:",i)
